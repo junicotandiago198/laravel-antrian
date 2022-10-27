f03bbb41f5e078c35912fc06dc86da62fef57daf
junicotandiago198 <junicotandiagoo21@gmail.com>
2022-10-27 14:58:19 +0700

    create function teller1 and teller2 +1

--- app/Http/Controllers/AntrianController.php
+++ app/Http/Controllers/AntrianController.php
@@ -24,4 +24,22 @@ class AntrianController extends Controller
         $data['antri'] = "cs";
         return view('antrian', $data);
     }
+
+    public function teller1($cs,$teller1,$teller2)
+    {
+        $data['antrian_cs'] = $cs;
+        $data['antrian_teller1'] = $teller1+1;
+        $data['antrian_teller2'] = $teller2;
+        $data['antri'] = "tl1";
+        return view('antrian', $data);
+    }
+
+    public function teller2($cs,$teller1,$teller2)
+    {
+        $data['antrian_cs'] = $cs;
+        $data['antrian_teller1'] = $teller1;
+        $data['antrian_teller2'] = $teller2+1;
+        $data['antri'] = "tl2";
+        return view('antrian', $data);
+    }
 }
